5cd25575abefff795e22983f826e5795ddf588a7
Viktor <example@gmail.com>
2020-10-30 13:43:51 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+console.log(4 + + "5");
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(44%25);
+
+console.log(2 + 2 * 2 != '6');
+
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked && isClose);
